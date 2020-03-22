19a0d56f16e43ab583adbf70e733d025d59a4018
PratyushAnand08 <PratyushAnand08@gmail.com>
2020-03-21 23:47:05 -0400

    Implementing Strategy Design Pattern

--- StrategyDesignPattern/Program.cs
+++ StrategyDesignPattern/Program.cs
@@ -1,12 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StrategyDesignPattern
 {
+
+    public interface IStrategy
+    {
+        object DoAlgorithm(object data);
+    }
+
+    class ConcreteStrategyA : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+
+            list.Sort();
+
+            return list;
+        }
+    }
+
+    class ConcreteStrategyB : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+
+            list.Sort();
+            list.Reverse();
+
+            return list;
+        }
+    }
+
+    class Context
+    {
+        private IStrategy strategy;
+
+        public Context() { }
+
+        public Context(IStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public void SetStrategy(IStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Context: Sorting Data using strategy...");
+
+            var result = this.strategy.DoAlgorithm(new List<string> { "a", "b", "c", "d", "e" });
+
+            var resultStr = "";
+
+            foreach (var element in result as List<string>)
+            {
+                resultStr += element + ",";
+            }
+
+            Console.WriteLine(resultStr);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var context = new Context();
+
+            Console.WriteLine("Sorting the string in normal manner...");
+            context.SetStrategy(new ConcreteStrategyA());
+            context.DoSomething();
+
+            Console.WriteLine("Sorting the string in normal manner...");
+            context.SetStrategy(new ConcreteStrategyB());
+            context.DoSomething();
         }
     }
 }
